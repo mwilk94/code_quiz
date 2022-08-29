25a62bce2165caa26aec4d47cc3d9ede5fd7085a
Coding <coding-profile@oscarmeishasmbp.mynetworksettings.com>
2022-08-29 10:17:47 -0400

    declared variables

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ var button_A = document.getElementById("buttonA");
 var button_B = document.getElementById("buttonB");
 var button_C = document.getElementById("buttonC");
 var button_D = document.getElementById("buttonD");
+
+var submitInitialBtn = document.getElementById("submit-initial");
+var playAgainBtn = document.getElementById("play-again");
+var initialInput = document.getElementById("initial");
+
+var mainPageEl = document.querySelector(".main-page");
+var quizInProgressEl = document.querySelector(".quiz-in-progress");
+var timerEl = document.querySelector(".timer");
+var questionsEl = document.querySelector(".questions");
+var answerCheckEl = document.querySelector(".answer-check");
+var quizOverEl = document.querySelector(".quiz-over");
+var finalScoreEl = document.querySelector(".final-score");
+var highScorePageEl = document.querySelector(".score-page");
+var scoreInitialEl = document.querySelector(".score-initial");
+
+
+// Declare and Initialize variables
+var timerCountDown = 60;
+var questionIndex = 0;
+var score = 0;
+var timerInterval;
