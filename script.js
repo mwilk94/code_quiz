2814b63c5e2e51d4699635fe455e5f605f8499cd
Coding <coding-profile@oscarmeishasmbp.mynetworksettings.com>
2022-08-29 10:05:16 -0400

    completed html and added variables to javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+var startButton = document.getElementById("start-button");
+var highScoreButton = document.getElementById("highscore-button");
+
+var button_A = document.getElementById("buttonA");
+var button_B = document.getElementById("buttonB");
+var button_C = document.getElementById("buttonC");
+var button_D = document.getElementById("buttonD");
